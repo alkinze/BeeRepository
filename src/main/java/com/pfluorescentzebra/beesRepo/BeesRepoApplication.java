a7160141f00a9bf6e9eba9b51326a92f5609717d
Stephanie Polansky <alkinze@gmail.com>
2020-06-11 11:01:25 -0500

    initial commit with test folder structure

---
+++ src/main/java/com/pfluorescentzebra/beesRepo/BeesRepoApplication.java
@@ -0,0 +1,13 @@
+package com.pfluorescentzebra.beesRepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeesRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeesRepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pfluorescentzebra/beesRepo/BeesRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pfluorescentzebra.beesRepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeesRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
